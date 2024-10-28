6be0b635c61ae2cee183c5a6f1b3095c38de569a
satyam <satyamg650@gmail.com>
2024-10-28 23:21:41 +0530

    Implemented frontend in react

---
+++ frontend/src/components/Application.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Application.css';
+
+const Application: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>Welcome to the application</h1>
+    </div>
+  );
+};
+
+export default Application;
